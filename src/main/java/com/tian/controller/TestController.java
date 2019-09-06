99040353e1f88f83ac049510b20654eba68a9cb5
tianxiong <tianxiong@akulaku.com>
2019-09-06 15:22:43 +0800

    基于springboot2.0+实现的springcloud的config功能.

--- src/main/java/com/tian/controller/TestController.java
+++ src/main/java/com/tian/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.tian.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
  * Creaed by Administrator on 2018/7/31 0031.
  */
 @RestController
+@RefreshScope
 public class TestController {
+    @Value("${test}")
+    private String test;
+
     @GetMapping("/test")
     public String test(){
-        return "this is test.";
+        return "this is test: "+ test;
     }
 }
